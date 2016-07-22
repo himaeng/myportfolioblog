fc9d5abcf9e3f4aba523a5cd1b966394b9c75db6
himaeng <him_aeng@yahoo.co.th>
2016-07-22 16:01:39 +0700

    Add edit & destroy project alilities

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -23,6 +23,22 @@ class ProjectsController < ApplicationController
 
 	end
 
+	def edit
+	end
+
+	def update
+		if @project.update project_params
+			redirect_to @project, notice: "Nice Him! that project was successfully updated"
+		else
+			render :edit
+		end
+	end
+
+	def destroy
+		@project.destroy
+		redirect_to projects_path
+	end
+
 	private
 
 	def project_params
