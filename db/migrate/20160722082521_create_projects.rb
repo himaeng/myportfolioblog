109475ea7e0cb89fce1691b13fe4a4cd8261c1f3
himaeng <him_aeng@yahoo.co.th>
2016-07-22 15:49:54 +0700

    Setup & styled basic project functionality

---
+++ db/migrate/20160722082521_create_projects.rb
@@ -0,0 +1,12 @@
+class CreateProjects < ActiveRecord::Migration
+  def change
+    create_table :projects do |t|
+      t.string :title
+      t.string :description
+      t.string :text
+      t.string :link
+
+      t.timestamps null: false
+    end
+  end
+end
